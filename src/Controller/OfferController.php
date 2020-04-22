b4e1bf3b61f5957bab6330b9be1c8d771b4b2b66
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2020-04-22 17:41:39 -0300

    Fixing back OfferController to its natural scope

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Company;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Entity\Offer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,18 +10,28 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class OfferController
  * @package App\Controller
- * @IsGranted("ROLE_COMPANY")
+ * @Route("/offer")
  */
 class OfferController extends AbstractController
 {
     /**
-     * @Route("/company/{id}/offers", name="offers")
+     * @Route("/", name="offers")
      * @ParamConverter("company", class="App\Entity\Company")
      */
-    public function index(Company $company)
+    public function index()
     {
-        return $this->render('offer/index.html.twig', [
-            'offers' => $company->getOffers(),
+        return $this->render('offer/index.html.twig');
+    }
+
+    /**
+     * @Route("/offer/{id}", name="offer_show")
+     * @ParamConverter("offer", class="App\Entity\Offer")
+     */
+    public function show(Offer $offer)
+    {
+        return $this->render('offer/show.html.twig',
+        [
+            'offer' => $offer,
         ]);
     }
 }
